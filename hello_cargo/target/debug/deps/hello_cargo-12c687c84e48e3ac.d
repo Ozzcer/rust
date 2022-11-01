cbb5ee0df97428d131e7b83b06802c33a05ca96e
Oliver Harrison <oliver.harrison@miomni.com>
2022-11-01 14:48:18 +0000

    hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-12c687c84e48e3ac.d
@@ -0,0 +1,5 @@
+/home/oz/repos/rust/hello_cargo/target/debug/deps/hello_cargo-12c687c84e48e3ac.rmeta: src/main.rs
+
+/home/oz/repos/rust/hello_cargo/target/debug/deps/hello_cargo-12c687c84e48e3ac.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/oz/repos/rust/hello_cargo/target/debug/hello_cargo: /home/oz/repos/rust/hello_cargo/src/main.rs
